7ba05b6a2ae41480890fe2ff061ada0edcb722e7
tripleaceinnovations <43879899+tripleaceinnovations@users.noreply.github.com>
2019-08-21 16:59:43 -0400

    Update models_test.go

--- models/models_test.go
+++ models/models_test.go
@@ -2,14 +2,14 @@ package models
 
 import (
 	"testing"
-
-	"github.com/tripleaceinnovations/api/models"
+	//"github.com/tripleaceinnovations/learngo/models"
 )
 
 //Test IsPalindrome function
-func testIsPalindromeEmptyArg(t *testing.T) {
+func TestIsPalindromeEmptyArg(t *testing.T) {
 	//test for empty argument
-	emptyResult := models.IsPalindrome("")
+	//emptyResult := models.IsPalindrome("")
+	emptyResult := IsPalindrome("")
 
 	if emptyResult != false {
 		t.Errorf("IsPalindrome function failed, expected `%v`, got `%v`", "false", emptyResult)
@@ -19,8 +19,9 @@ func testIsPalindromeEmptyArg(t *testing.T) {
 }
 
 //test for valid postive arguments
-func testIsPalindromeValidPosArg(t *testing.T) {
-	postiveresult := models.IsPalindrome("level")
+func TestIsPalindromeValidPosArg(t *testing.T) {
+	//postiveresult := models.IsPalindrome("level")
+	postiveresult := IsPalindrome("level")
 
 	if postiveresult != true {
 		t.Errorf("IsPalindrome function failed, expected `%v`, got `%v`", "true", postiveresult)
@@ -31,8 +32,9 @@ func testIsPalindromeValidPosArg(t *testing.T) {
 }
 
 //test for valid negative arguments
-func testIsPalindromeValidNegArg(t *testing.T) {
-	negativeresult := models.IsPalindrome("bash")
+func TestIsPalindromeValidNegArg(t *testing.T) {
+	//negativeresult := models.IsPalindrome("test")
+	negativeresult := IsPalindrome("test")
 
 	if negativeresult != false {
 		t.Errorf("IsPalindrome function failed, expected `%v`, got `%v`", "false", negativeresult)
