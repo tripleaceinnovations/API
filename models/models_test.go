b950db81d31d3c6f030b2506c32a4128ddc500ab
Adebola Adekoya <adebola.adekoya80@gmail.com>
2019-08-21 21:26:57 -0400

    minor fixes

--- models/models_test.go
+++ models/models_test.go
@@ -5,22 +5,6 @@ import (
 	//"github.com/tripleaceinnovations/learngo/models"
 )
 
-<<<<<<< HEAD
-=======
-//Test IsPalindrome function
-func TestIsPalindromeEmptyArg(t *testing.T) {
-	//test for empty argument
-	//emptyResult := models.IsPalindrome("")
-	emptyResult := IsPalindrome("")
-
-	if emptyResult != false {
-		t.Errorf("IsPalindrome function failed, expected `%v`, got `%v`", "false", emptyResult)
-	} else {
-		t.Logf("IsPalindrome function success, expected `%v`, got `%v`", "false", emptyResult)
-	}
-}
-
->>>>>>> 7ba05b6a2ae41480890fe2ff061ada0edcb722e7
 //test for valid postive arguments
 func TestIsPalindromeValidPosArg(t *testing.T) {
 	//postiveresult := models.IsPalindrome("level")
